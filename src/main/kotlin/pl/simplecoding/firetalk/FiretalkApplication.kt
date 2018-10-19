d6f24881b87c23b2df46ef524e867cd0d8006df8
ArturC <artur.m.czopek@metrosoft.com>
2018-10-19 16:38:05 +0200

    01. STRUCTURE - init

---
+++ src/main/kotlin/pl/simplecoding/firetalk/FiretalkApplication.kt
@@ -0,0 +1,15 @@
+package pl.simplecoding.firetalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * @Author Artur Czopek
+ * @Link https://simplecoding.pl/firetalk
+ */
+
+@SpringBootApplication
+class FiretalkApplication
+fun main(args: Array<String>) {
+    runApplication<FiretalkApplication>(*args)
+}
