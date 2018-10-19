3e748b2008ebc5a0108a54cbb82cb2b88803b8ea
ArturC <artur.m.czopek@metrosoft.com>
2018-10-19 17:28:25 +0200

    08. TOKEN_TYPE - enum, companion, when

---
+++ src/main/kotlin/pl/simplecoding/firetalk/TokenType.kt
@@ -0,0 +1,15 @@
+package pl.simplecoding.firetalk
+
+import java.lang.UnsupportedOperationException
+
+enum class TokenType {
+    GREEN, RED;
+
+    companion object {
+        fun fromString(value: String) = when (value) {
+            "green" -> GREEN
+            "red" -> RED
+            else -> throw UnsupportedOperationException("There is no token like $value")
+        }
+    }
+}
