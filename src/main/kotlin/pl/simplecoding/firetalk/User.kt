4311586eda46d14ac7cdffd9c96de71c688e9b45
ArturC <artur.m.czopek@metrosoft.com>
2018-10-19 17:23:18 +0200

    07. USER_INSERT_RUNNER - vararg, named params, foreach (1..20), string $

--- src/main/kotlin/pl/simplecoding/firetalk/User.kt
+++ src/main/kotlin/pl/simplecoding/firetalk/User.kt
@@ -1,7 +1,10 @@
 package pl.simplecoding.firetalk
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.boot.CommandLineRunner
 import org.springframework.data.repository.CrudRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -26,4 +29,32 @@ interface UserRepository : CrudRepository<User, Long> {
     override fun findAll(): List<User>
 
     fun findOneByName(name: String): User?
+}
+
+
+@Service
+class UserInsertRunner(
+        private val userRepository: UserRepository,
+        private val authorityRepository: AuthorityRepository,
+        private val bCryptPasswordEncoder: BCryptPasswordEncoder
+): CommandLineRunner {
+    override fun run(vararg args: String?) {
+
+        (1..20).forEach {
+            val id = it.toLong()
+            val name = "user$id"
+
+            val authority = Authority(id, name, "user")
+            authorityRepository.save(authority)
+
+            val user = User(
+                    name = name,
+                    password = bCryptPasswordEncoder.encode(name),
+                    id = id,
+                    authorityId = id
+            )
+
+            userRepository.save(user)
+        }
+    }
 }
